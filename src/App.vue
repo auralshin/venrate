71d8a3c1d691580479c16e5a3ee0aeb4d3f62f90
auralshin <anshulspartan141@gmail.com>
2021-04-21 06:04:21 +0530

    login screen added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,31 @@
 <template>
-  <div id="app">
-    <button @click="auth" >Login</button>
+  <div id="app" class="center">
+    <div class="heading">
+      <h1>Ethereum India Fellowship 2.0</h1>
+    </div>
+    <div class="card">
+  <div class="card-image">
+    <figure class="image is-4by3">
+      <img src="https://jaxenter.com/wp-content/uploads/2018/07/shutterstock_781956604.jpg" alt="Placeholder image">
+    </figure>
+  </div>
+  <div class="card-content">
+    <div class="media">
+      <div class="media-left">
+        <figure class="image is-48x48">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Vitalik_Buterin_TechCrunch_London_2015.jpg/640px-Vitalik_Buterin_TechCrunch_London_2015.jpg" alt="Placeholder image">
+        </figure>
+      </div>
+      <div class="media-content">
+        <p class="title is-4">Vitalik Buterin</p>
+        <p class="subtitle is-6">@VitalikButerin</p>
+      </div>
+    </div>
+    <div class="center">
+      <b-button type="is-primary" rounded size="is-medium" icon-left="ethereum" @click="auth">Login</b-button>
+    </div>
+  </div>
+</div>
   </div>
 </template>
 
@@ -30,12 +55,33 @@ export default {
 </script>
 
 <style>
+html {
+  background: #fff;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  flex-direction: column;
+  color: #fdfdfd;
+  
+  padding: 0;
+  height: 100vh;
+  
+  background-color: #0cbaba;
+  background-image: linear-gradient(315deg, #0cbaba 0%, #380036 74%);
+  /* background-color: #0cbaba;
+background-image: linear-gradient(315deg, #0cbaba 0%, #380036 74%);
+ */
+
+
+}
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.heading {
+  padding-top: 60px ;
 }
 </style>
