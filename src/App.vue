e516afa9edacac32a71b3429866e0ed1f84ca1e9
auralshin <anshulspartan141@gmail.com>
2021-04-21 06:13:03 +0530

    login refactored

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,14 @@
 <template>
   <div id="app" class="center">
     <div class="heading">
-      <h1>Ethereum India Fellowship 2.0</h1>
+      <p>Ethereum India Fellowship 2.0</p>
     </div>
-    <div class="card">
-  <div class="card-image">
-    <figure class="image is-4by3">
-      <img src="https://jaxenter.com/wp-content/uploads/2018/07/shutterstock_781956604.jpg" alt="Placeholder image">
-    </figure>
-  </div>
-  <div class="card-content">
-    <div class="media">
-      <div class="media-left">
-        <figure class="image is-48x48">
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Vitalik_Buterin_TechCrunch_London_2015.jpg/640px-Vitalik_Buterin_TechCrunch_London_2015.jpg" alt="Placeholder image">
-        </figure>
-      </div>
-      <div class="media-content">
-        <p class="title is-4">Vitalik Buterin</p>
-        <p class="subtitle is-6">@VitalikButerin</p>
-      </div>
-    </div>
-    <div class="center">
-      <b-button type="is-primary" rounded size="is-medium" icon-left="ethereum" @click="auth">Login</b-button>
-    </div>
-  </div>
-</div>
+    <Card />
   </div>
 </template>
 
 <script>
+import Card from './components/cardLogin'
 import CeramicClient from '@ceramicnetwork/http-client'
 import { ThreeIdConnect,  EthereumAuthProvider } from '3id-connect'
 const API_URL = "https://ceramic-clay.3boxlabs.com"
@@ -37,7 +16,7 @@ const ceramic = new CeramicClient(API_URL)
 export default {
   name: 'App',
   components: {
-    
+    Card,
   },
   methods: {
     async auth() {
@@ -81,7 +60,5 @@ background-image: linear-gradient(315deg, #0cbaba 0%, #380036 74%);
   justify-content: center;
   align-items: center;
 }
-.heading {
-  padding-top: 60px ;
-}
+
 </style>
---
+++ src/components/cardLogin.vue
@@ -0,0 +1,43 @@
+<template>
+ <div class="card">
+  <div class="card-image">
+    <figure class="image is-4by3">
+      <img src="https://jaxenter.com/wp-content/uploads/2018/07/shutterstock_781956604.jpg" alt="Placeholder image">
+    </figure>
+  </div>
+  <div class="card-content">
+    <div class="media">
+      <div class="media-left">
+        <figure class="image is-48x48">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Vitalik_Buterin_TechCrunch_London_2015.jpg/640px-Vitalik_Buterin_TechCrunch_London_2015.jpg" alt="Placeholder image">
+        </figure>
+      </div>
+      <div class="media-content">
+        <p class="title is-4">Vitalik Buterin</p>
+        <p class="subtitle is-6">@VitalikButerin</p>
+      </div>
+    </div>
+    <div class="center">
+      <b-button type="is-primary" rounded size="is-medium" icon-left="ethereum" @click="auth">Login</b-button>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
