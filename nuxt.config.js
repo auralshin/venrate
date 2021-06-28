cd36b85474eb05bb225d00a952f4f23542182fb8
Aayush Kumar <r3ptoid1@gmail.com>
2021-06-28 20:11:45 +0530

    Added graph ql and thegraph.com

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,32 +1,38 @@
-import colors from 'vuetify/es5/util/colors'
+import colors from "vuetify/es5/util/colors";
 
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - venrate',
-    title: 'venrate',
+    titleTemplate: "%s - venrate",
+    title: "venrate",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en"
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      {rel:'stylesheet',href:'https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600&display=swap' },
-      {rel:'stylesheet',href:'https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500&display=swap' },
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600&display=swap"
+      },
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500&display=swap"
+      }
     ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -34,16 +40,15 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
+    "@nuxtjs/vuetify"
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: ["@nuxtjs/apollo"],
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: ["~/assets/variables.scss"],
     theme: {
       dark: true,
       themes: {
@@ -60,7 +65,21 @@ export default {
     }
   },
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
+  apollo: {
+    clientConfigs: {
+      default: {
+        httpEndpoint: "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2",
+        httpLinkOptions: {
+          credentials: "same-origin"
+        }
+      },
+      aawe:{
+        httpEndpoint:""
+      }
+    }
   }
+};
+
+// Build Configuration: https://go.nuxtjs.dev/config-build
+build: {
 }
---
+++ pages/test.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>Testing</h1>
+    <ul class="ul" style="color:white">
+      <li v-for="data in data" :key="data.id">
+        {{ data.id }}
+      </li>
+      <!-- {{
+        data[0]
+      }} -->
+    </ul>
+  </div>
+</template>
+
+<script>
+import gql from "graphql-tag";
+export default {
+  //   data() {
+  //     return {
+  //       datas: []
+  //     };
+  //   },
+  apollo: {
+    // $client:'aawe',
+    data: gql`
+      query data {
+        data: pairs(first: 10) {
+          id
+        }
+      }
+    `
+  }
+};
+// console.log(this.data);
+</script>
+
+<style lang="scss" scoped>
+.ul {
+  margin-top: 20px;
+  margin-left: 20px;
+  padding: 20px;
+}
+</style>
